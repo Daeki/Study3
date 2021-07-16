dba70536df9e160a4fc5cf6119cb4b1a4aea1912
Daeki <kimdaeki@gmail.com>
2021-07-16 17:54:10 +0900

    a

---
+++ src/com/iu/s4/Array_8.java
@@ -0,0 +1,28 @@
+package com.iu.s4;
+
+public class Array_8 {
+	
+	public static void main(String[] args) {
+		int [] ids = {1234, 4567, 4356, 1258};
+		int [] pws = {6789, 5437, 1235, 9874};
+		
+		
+		//1. 로그인
+		//2. 회원가입
+		//3. 종료
+		
+		
+		//1번 로그인 성공 하면 메인메뉴가
+		//1. 로그아웃
+		//2. 회원탈퇴
+		//3. 종   료
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
